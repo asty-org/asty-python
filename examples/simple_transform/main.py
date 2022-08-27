8866cf99484f299ff470c5392ce55632ef8f3abe
Eugene Chernyshov <chernyshov.eugene@gmail.com>
2022-08-28 00:49:03 +0300

    added simple transform example

---
+++ examples/simple_transform/main.py
@@ -0,0 +1,59 @@
+import os.path
+
+from asty.nodes import (
+    CallExprNode,
+    FileNode,
+    IdentNode,
+    SelectorExprNode,
+)
+from asty.utils import run_with_container
+from asty.visitors import NodeTransformer
+
+
+class TestTransformer(NodeTransformer):
+    def visit_CallExprNode(self, node):
+        match node:
+            case CallExprNode(
+                fun=IdentNode(name='print'),
+                args=args,
+            ):
+                return CallExprNode.construct(
+                    fun=SelectorExprNode.construct(
+                        x=IdentNode.construct(name='log'),
+                        sel=IdentNode.construct(name='Print'),
+                    ),
+                    args=args,
+                )
+        return super().generic_visit(node)
+
+
+def main():
+    root = os.path.dirname(__file__)
+    result = run_with_container(
+        ["go2json", "-input", "/var/data/test.go"],
+        volumes={root: "/var/data"},
+    )
+    if result.returncode != 0:
+        raise Exception(result.stderr.decode("utf-8"))
+
+    tree = FileNode.parse_raw(result.stdout)
+
+    transformer = TestTransformer()
+    tree = transformer.visit(tree)
+
+    data = tree.json(
+        exclude_unset=True,
+        by_alias=True,
+    )
+    result = run_with_container(
+        ["json2go"],
+        input=data.encode("utf-8"),
+    )
+    if result.returncode != 0:
+        raise Exception(result.stderr.decode("utf-8"))
+
+    print(result.stdout.decode("utf-8"))
+
+
+if __name__ == "__main__":
+    main()
